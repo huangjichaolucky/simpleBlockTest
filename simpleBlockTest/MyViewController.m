30c5ae4389a9535306196e6a6355c4bfd5e1e7fc
huangjichaolucky <huangjichaolucky@126.com>
2015-07-22 09:06:55 +0800

    第二次加注释

--- simpleBlockTest/MyViewController.m
+++ simpleBlockTest/MyViewController.m
@@ -20,7 +20,9 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 }
-
+/**
+ *  又加上一行注释
+ */
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
